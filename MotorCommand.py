0ccf271080e4dedaa3e42de91c6bea825f2e8505
Valentina Rojas <valenrojas@ug.uchile.cl>
2023-01-19 18:11:15 -0300

    structure to turn on led

--- MotorCommand.py
+++ MotorCommand.py
@@ -4,21 +4,21 @@ import struct
 
 class MotorCommand(object):
 
-    _struct = struct.Struct('<Bh')
+    _struct = struct.Struct('<BH')
 
     def __init__(self):
-        self.id = 0 #unsigned char
-        self.cmd = 0 #unsigned char
+        self.cmd = 0 #unsigned char - 1 byte
+        self.data = 0 #unsigned short - 2 byte
 
     def serialize(self, buff):
         #try
-        buff.write(MotorCommand._struct.pack(self.id, self.cmd))
+        buff.write(MotorCommand._struct.pack(self.cmd, self.data))
         #except struct.error as se:
         #    raise SerializationError('Error in serialization %s' % (self.__str__))
 
     def fromTuple(self, data):
-        self.id = data.id
         self.cmd = data.cmd
+        self.data = data.data
 
 def to_hex(data):
     return ":".join("{:02x}".format(c) for c in data)
